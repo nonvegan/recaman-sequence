1cb7556cb80dd2e98cb9cdbbaa9fb729220dfb9b
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-02-26 19:28:10 +0000

    helpers save

--- js/helpers.js
+++ js/helpers.js
@@ -4,19 +4,5 @@ function getMs(fps) {
 function lerp(v0, v1, t) {
   return v0*(1-t)+v1*t
 }
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-function mapValue(n, start1, stop1, start2, stop2, withinBounds) {
-  const newval = ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
-  if (!withinBounds) {
-    return newval;
-  }
-  if (start2 < stop2) {
-    return constrain(newval, start2, stop2);
-  } else {
-    return constrain(newval, stop2, start2);
-  }
-}
-export { getMs,lerp ,random,mapValue};
+export { getMs,lerp};
